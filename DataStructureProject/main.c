547ece6630b105c612a743fae8cd95e5429b424f
syj <syj990930@naver.com>
2021-10-14 17:59:52 +0900

    Initial Commit

---
+++ DataStructureProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DataStructureProject
+//
+//  Created by 서영정 on 2021/10/14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
